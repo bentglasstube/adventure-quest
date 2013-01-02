10af14d94fd4bad9e8bdc6389c092734d2c3f615
Alan Berndt <alan@eatabrick.org>
2013-01-02 16:15:24 -0700

    Switch to versioned archive format, remove duplicates from legacy format

--- src/net/honeybadgerlabs/adventurequest/QuestAdapter.java
+++ src/net/honeybadgerlabs/adventurequest/QuestAdapter.java
@@ -1,6 +1,7 @@
 package net.honeybadgerlabs.adventurequest;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -13,10 +14,13 @@ import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 
 public class QuestAdapter extends ArrayAdapter<Quest> {
+  private final static String TAG = "QuestAdapter";
   private final static String FILE = "archives.dat";
+  private final static int MAGIC = 0xB00B;
 
   public QuestAdapter(Context context) {
     super(context, android.R.id.empty);
@@ -55,16 +59,68 @@ public class QuestAdapter extends ArrayAdapter<Quest> {
       BufferedInputStream buf = new BufferedInputStream(file);
       DataInputStream in = new DataInputStream(buf);
 
-      while (in.available() > 0) {
-        int status = in.readInt();
-        String description = in.readUTF();
+      in.mark(8);
+      int magic = in.readInt();
 
-        add(new Quest(description, status));
+      if (magic == MAGIC) {
+        int version = in.readInt();
+        Log.d(TAG, "Reading archives version " + version);
+
+        switch (version) {
+          case 1:
+            loadVersion1(in);
+            break;
+          default:
+            Log.d(TAG, "Uknown archive version");
+            break;
+        }
+      } else {
+        Log.d(TAG, "Reading legacy archives");
+
+        in.reset();
+        loadLegacy(in);
       }
 
       in.close();
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  private void loadLegacy(DataInputStream in) throws IOException {
+    HashSet set = new HashSet<String>(500);
+    int dupes = 0;
+    int bogus = 0;
+
+    while (in.available() > 0) {
+      int status = in.readInt();
+      String description = in.readUTF();
+
+      switch (status) {
+        case Quest.STATUS_ABANDON:
+        case Quest.STATUS_FAILED:
+        case Quest.STATUS_COMPLETE:
+          if (set.add(description)) {
+            add(new Quest(description, status));
+          } else {
+            dupes++;
+          }
+          break;
+        default:
+          bogus++;
+      }
+    }
+
+    Log.d(TAG, "Skipped " + dupes + " duplicates and " + bogus + " bogus quests");
+  }
+
+  private void loadVersion1(DataInputStream in) throws IOException {
+    int count = in.readInt();
+    for (int i = 0; i < count; ++i) {
+      int status = in.readInt();
+      String description = in.readUTF();
+
+      add(new Quest(description, status));
     }
   }
 
@@ -74,7 +130,14 @@ public class QuestAdapter extends ArrayAdapter<Quest> {
       BufferedOutputStream buf = new BufferedOutputStream(file);
       DataOutputStream out = new DataOutputStream(buf);
 
-      for (int i = 0; i < getCount(); i++) {
+      out.writeInt(MAGIC);
+      out.writeInt(1);
+
+      int count = getCount();
+
+      out.writeInt(count);
+
+      for (int i = 0; i < count; i++) {
         Quest q = getItem(i);
         out.writeInt(q.status);
         out.writeUTF(q.description);
